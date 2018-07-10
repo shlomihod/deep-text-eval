be029d86ea254c663d0a3dc53e38b4c6f030d85e
Maximilian Seidler <maxseidler@gmx.net>
2018-07-10 15:26:24 +0200

    Dummy file for top 10 feature extaction

---
+++ benchmark_model/top10WeeBit.py
@@ -0,0 +1,2 @@
+test_string = """Max excels at large-scale information extraction tasks. It's written from the ground up in carefully memory-managed Cython. Independent research has confirmed that Max is the fastest in the world. If your application needs to process entire web dumps, Max is the library you want to be using."""
+
