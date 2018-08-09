ca65be694347f09df32318a7f268076fb0836f70
shlomihod <hod_s@bezeqint.net>
2018-08-09 20:36:39 +0000

    end to end run

--- run_text2features2prediction.sh
+++ run_text2features2prediction.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
+
+usage ()
+{
+  echo 'Usage : Script <corpus name>'
+  exit
+}
+
+if [ "$#" -ne 1 ]
+then
+  usage
+fi
+
+
 set -x
 set -e
 shopt -s expand_aliases
 
-export CUDA_VISIBLE_DEVICES=0
-cd ~/deep-text-eval/text2features/
-python3 nlp_extractor.py ../data/weebit/weebit.h5
-
 alias nbx='jupyter nbconvert --execute --to notebook --inplace --ExecutePreprocessor.timeout=-1'
 
-cd ~/deep-text-eval/text2features/
+export CUDA_VISIBLE_DEVICES=0
+
+cd ~/deep-text-eval/text2features
+python3 nlp_extractor.py "../data/$1/$1.h5"
+
+cd ~/deep-text-eval/text2features/$1
 nbx features_analysis.ipynb
 
-cd ~/deep-text-eval/features2prediction/weebit/
+cd ~/deep-text-eval/features2prediction/$1
 nbx classification.ipynb
 nbx regression.ipynb
 nbx ordinal-regression.ipynb
 
 nbx models-comparison.ipynb
+nbx features-selection.ipynb
 
 cd ~/deep-text-eval/
